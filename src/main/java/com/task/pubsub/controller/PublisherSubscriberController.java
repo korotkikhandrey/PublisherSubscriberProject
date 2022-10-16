ca03332855a37482ff461a891458523f703a06c2
Andrey Korotkikh <korotkikh.andrey@gmail.com>
2022-10-16 20:13:24 +0400

    security configuration - API key

--- src/main/java/com/task/pubsub/controller/PublisherSubscriberController.java
+++ src/main/java/com/task/pubsub/controller/PublisherSubscriberController.java
@@ -4,6 +4,7 @@ import com.task.pubsub.entity.Message;
 import com.task.pubsub.entity.Subscriber;
 import com.task.pubsub.service.PublisherSubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ import java.util.List;
  * Rest controller for publishing - subscribing process.
  */
 @RestController
-@RequestMapping("/pubsub")
+@RequestMapping(value = "/pubsub")
 public class PublisherSubscriberController {
 
     @Autowired
@@ -24,7 +25,8 @@ public class PublisherSubscriberController {
      * @param message
      */
     @PostMapping("/addMessageToQueue")
-    public void addMessageToQueue(@RequestBody String message) {
+    public void addMessageToQueue(@RequestHeader(name = "apikey") String apikey,
+                                  @RequestBody String message) {
         publisherSubscriberService.addMessageToQueue(message);
     }
     /**
@@ -35,7 +37,8 @@ public class PublisherSubscriberController {
                  consumes = MediaType.APPLICATION_JSON_VALUE,
                  produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public void addSubscriber(@RequestBody Subscriber subscriberName) {
+    public void addSubscriber(@RequestHeader(name = "apikey") String apikey,
+                              @RequestBody Subscriber subscriberName) {
         publisherSubscriberService.addSubscriber(subscriberName);
     }
 
@@ -44,7 +47,8 @@ public class PublisherSubscriberController {
      * @param id
      */
     @DeleteMapping(value="/removeSubscriber/{id}")
-    public void removeSubscriber(@PathVariable Long id) {
+    public void removeSubscriber(@RequestHeader(name = "apikey") String apikey,
+                                 @PathVariable Long id) {
         publisherSubscriberService.removeSubscriber(id);
     }
 
@@ -52,7 +56,7 @@ public class PublisherSubscriberController {
      * Broadcast all the messages.
      */
     @PostMapping("/broadcastMessages")
-    public void broadcastMessages() {
+    public void broadcastMessages(@RequestHeader(name = "apikey") String apikey) {
         publisherSubscriberService.broadcastMessages();
     }
     /**
@@ -61,7 +65,7 @@ public class PublisherSubscriberController {
      */
     @GetMapping(value = "/getAllMessages", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<Message> getAllMessages() {
+    public List<Message> getAllMessages(@RequestHeader(name = "apikey") String apikey) {
         return publisherSubscriberService.getAllMessagesFromDB();
     }
 
@@ -72,7 +76,8 @@ public class PublisherSubscriberController {
      */
     @GetMapping(value = "/getAllMessagesForSubscriber/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<Message> getAllMessagesForSubscriber(@PathVariable Long id) {
+    public List<Message> getAllMessagesForSubscriber(@RequestHeader(name = "apikey") String apikey,
+                                                     @PathVariable Long id) {
         return publisherSubscriberService.getAllMessagesForSubscriber(id);
     }
 }
