2b0d77e2add85a09cfb56fedc06393e11853ef5b
Andrey Korotkikh <korotkikh.andrey@gmail.com>
2022-10-18 15:49:27 +0400

    seq_id replaced by auto_increment for mysql, dropAllTruncate liquibase task added

--- src/main/java/com/task/pubsub/entity/AbstractEntity.java
+++ src/main/java/com/task/pubsub/entity/AbstractEntity.java
@@ -3,9 +3,12 @@ package com.task.pubsub.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -15,16 +18,7 @@ public class AbstractEntity {
 
     @Id
     @Column(nullable = false, unique = true)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hilo_sequence_generator")
-    @GenericGenerator(
-            name = "hilo_sequence_generator",
-            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-            parameters = {
-                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "seq_id"),
-                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
-                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "50"),
-                    @org.hibernate.annotations.Parameter(name = "optimizer", value = "hilo")
-            })
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "create_date", nullable = false)
